24795dea6ae9f8af29696688fe00cb1c00e2c1a4
mjziolko <mj@ziolko.dev>
2022-02-19 06:33:47 -0600

    chat changes

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import ReactNativeBlobUtil from 'react-native-blob-util';
+import { Bubble, GiftedChat, IMessage } from 'react-native-gifted-chat';
+import SoundPlayer from 'react-native-sound-player';
+
+import AccessoryBar from './AccessoryBar';
+
+const URL = 'https://lxya-mjz-lxya.vercel.app';
 
 function Chat() {
+  const [messages, setMessages] = React.useState<IMessage[]>([]);
+  const [isTyping, setIsTyping] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'type something...',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'lexia',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = async (msgs: IMessage[]) => {
+    setMessages((previousMessages: IMessage[]) => GiftedChat.append(previousMessages, msgs));
+
+    const res = await ReactNativeBlobUtil.config({
+      // fileCache: true,
+    }).fetch('POST', `${URL}/api/text`, {
+      'Content-Type': 'application/json',
+    }, JSON.stringify({ text: msgs[0].text, mp3: true }));
+    console.log(res);
+    console.log(res.path());
+    // SoundPlayer.playSoundFile(res.path(), 'mp3');
+
+    // console.log(msgs);
+    // const response = await fetch(`${URL}/api/text`, options);
+    // // console.log(await response.json());
+    // const blob: Blob = await response.blob();
+    // console.log(blob);
+  };
+
+  const onPress = (_context: object, message: IMessage) => {
+    console.log(message.text);
+  };
+
   return (
-    <View style={{ ...styles.chatBox }}>
-      <Text>Hi</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      onLongPress={onPress}
+      onSend={(msgs) => onSend(msgs)}
+      renderAccessory={() => <AccessoryBar />}
+      user={{ _id: 1 }}
+    />
   );
 }
 
